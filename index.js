6b9614c945012538bbdddd36f7dd937b0aaabf39
NE-SmallTown <nesmalltown@gmail.com>
2018-09-03 20:58:53 +0800

    ssss2

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import './index.less';
 
 render();
 
+console.log(1);
+
 function render () {
     const div = document.createElement('div');
     div.innerHTML = '<h1>Hello world icon：<i class="ne-modal icon-chenggong"></i> <div class="img"></div></h1>';
